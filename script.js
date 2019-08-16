145173efc994ca3c8cb507235619f7ed76f64a0a
George_Izot <urka94@bk.ru>
2019-08-17 00:32:20 +0300

    delet all

--- script.js
+++
@@ -1,7 +0,0 @@
-let som
-console.log(som);
-//alert("hello world");
-//let answer = confirm("Ayou here?");
-//console.log(answer);
-let answer = prompt("Есть ли вам 18?", "");
-console.log(typeof(answer));
