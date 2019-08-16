3ca702b6316b20397cdd2dff5b919c7a806ed742
George_Izot <urka94@bk.ru>
2019-08-16 16:41:44 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let som
+console.log(som);
+//alert("hello world");
+//let answer = confirm("Ayou here?");
+//console.log(answer);
+let answer = prompt("Есть ли вам 18?", "");
+console.log(typeof(answer));
